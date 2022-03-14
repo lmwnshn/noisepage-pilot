78145c7056dc534f6e3c19865daca52730edf734
Wan Shen Lim <wanshen.lim@gmail.com>
2022-03-13 20:14:18 -0400

    moo

--- dodos/benchbase.py
+++ dodos/benchbase.py
@@ -70,7 +70,9 @@ def task_benchbase_build():
             # Compile BenchBase.
             "./mvnw clean package -Dmaven.test.skip=true -P postgres",
             lambda: os.chdir("target"),
+            "mv benchbase-2021-SNAPSHOT.tgz benchbase-postgres.tgz",  # P1 HACK
             "tar xvzf benchbase-postgres.tgz",
+            "mv benchbase-2021-SNAPSHOT benchbase-postgres",  # P1 HACK
             # Move artifacts out.
             lambda: os.chdir(doit.get_initial_workdir()),
             f"mv {BUILD_PATH / f'target/benchbase-postgres/*'} {ARTIFACTS_PATH}",
